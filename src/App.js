75d9b3efe548db3e9a481b08951254121888eae6
B-kumaraswamy <kumaraswamy491@gmail.com>
2024-03-02 12:54:09 +0530

    adding frontend folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import SignupAndLogin from "./components/signupandlogin";
 
-function App() {
+import OnSignUpComponent from "./components/signup";
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import NotFound from "./components/notfound";
+
+import HomeComponent from "./components/home";
+import ProductComponent from "./components/products";
+import Admin from "./components/admin";
+import { Authprovider } from "./components/context.js";
+import ProtectedRoute from "./components/protectedroute.js";
+import AddressComponent from "./components/address.js";
+// import { useAuthContext } from "./components/context.js";
+// import Cookie from "js-cookie";
+// import { useEffect } from "react";
+
+
+
+
+function OnApp() {
+  // const { setLoggedIn } = useAuthContext();
+
+  // useEffect(() => {
+  //   const token = Cookie.get('jwt-token');
+  //   if (token) {
+  //     // Check token validity (e.g., expiration) here if needed
+  //     setLoggedIn(true);
+  //   }
+  // }, [setLoggedIn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <Authprovider>
+     <BrowserRouter>
+    <Routes>
+    
+      <Route exact path = '/' element = {<SignupAndLogin/>}/>
+      <Route  exact  path = '/signup' element = {<OnSignUpComponent/>}/>
+      <Route exact  path = '/home' element = {<ProtectedRoute component = {HomeComponent}/>}/>
+      <Route  exact  path = '/products' element = {<ProtectedRoute component = {ProductComponent}/>}/>
+      <Route  exact  path = '/admin' element = {<ProtectedRoute component = {Admin}/>}/>
+      <Route exact path = '/address' element = {<AddressComponent/>}/>
+     
+      <Route path = '*' element = {<NotFound/>}/>
+      
+    </Routes>
+    </BrowserRouter>
+    </Authprovider>
+   
+    
+  )
 }
 
-export default App;
+
+export default OnApp;
+
+
+
+
